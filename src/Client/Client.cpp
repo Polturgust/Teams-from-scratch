57ba3d094918f660f5682e24de89bfa8ec36282a
Soha Larbi <soha.larbi@epitech.eu>
2026-04-16 10:23:44 +0200

    add /help

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -270,3 +270,27 @@ void Client::_dispatch(const ParsedCommand &cmd)
 
     std::cerr << "Unknown command: /" << cmd.name << ". Type /help for help." << std::endl;
 }
+
+void Client::_cmd_help()
+{
+    std::cout <<
+        "Available commands:\n"
+        "  /help                                        Show this help\n"
+        "  /login \"user_name\"                           Log in to the server\n"
+        "  /logout                                      Disconnect from the server\n"
+        "  /users                                       List all users\n"
+        "  /user \"user_uuid\"                            Get details about a user\n"
+        "  /send \"user_uuid\" \"message_body\"             Send a private message\n"
+        "  /messages \"user_uuid\"                        List messages with a user\n"
+        "  /subscribe \"team_uuid\"                       Subscribe to a team\n"
+        "  /subscribed [\"team_uuid\"]                    List subscribed teams or team members\n"
+        "  /unsubscribe \"team_uuid\"                     Unsubscribe from a team\n"
+        "  /use [\"team_uuid\" [\"channel_uuid\" [\"thread_uuid\"]]]  Set context\n"
+        "  /create [args...]                            Create resource (context-dependent)\n"
+        "    (no context)          \"team_name\" \"team_description\"\n"
+        "    (team)                \"channel_name\" \"channel_description\"\n"
+        "    (team+channel)        \"thread_title\" \"thread_body\"\n"
+        "    (team+channel+thread) \"comment_body\"\n"
+        "  /list                                        List resources (context-dependent)\n"
+        "  /info                                        Show info (context-dependent)\n";
+}
