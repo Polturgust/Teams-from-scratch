16e4f4e13271cebcb7de06e07fe7797e7331334e
Soha Larbi <soha.larbi@epitech.eu>
2026-04-16 10:16:56 +0200

    add main loop

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -95,3 +95,36 @@ void Client::_connect(const std::string &ip, int port)
         exit(84);
     }
 }
+
+void Client::run()
+{
+    _running = true;
+    while (_running) {
+        struct pollfd fds[2];
+        fds[0].fd     = STDIN_FILENO;
+        fds[0].events = POLLIN;
+        fds[1].fd     = _fd;
+        fds[1].events = POLLIN | (_send_buf.empty() ? 0 : POLLOUT);
+
+        int ret = poll(fds, 2, -1);
+        if (ret < 0) {
+            if (errno == EINTR) continue;
+            std::cerr << "poll: " << strerror(errno) << std::endl;
+            break;
+        }
+
+        if (fds[0].revents & POLLIN)
+            _handle_stdin();
+
+        if (fds[1].revents & POLLOUT)
+            _handle_server_write();
+
+        if (fds[1].revents & POLLIN)
+            _handle_server_read();
+
+        if (fds[1].revents & (POLLHUP | POLLERR)) {
+            std::cerr << "Server disconnected." << std::endl;
+            _running = false;
+        }
+    }
+}
