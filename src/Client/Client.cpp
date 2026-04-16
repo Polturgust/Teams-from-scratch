db3d7d768be6799c400a20e29c47c52c09ec1b63
Soha Larbi <soha.larbi@epitech.eu>
2026-04-16 10:12:54 +0200

    add serialisation helpers

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -5,3 +5,16 @@
 ** Client.cpp
 */
 
+#include "Client.hpp"
+#include "../../libs/myteams/logging_client.h"
+
+static void pack_fixed(std::vector<uint8_t> &buf, const std::string &s, size_t field_len)
+{
+    size_t copy_len = std::min(s.size(), field_len);
+    for (size_t i = 0; i < copy_len; ++i)
+        buf.push_back(static_cast<uint8_t>(s[i]));
+    for (size_t i = copy_len; i < field_len; ++i)
+        buf.push_back(0x00);
+}
+
+
